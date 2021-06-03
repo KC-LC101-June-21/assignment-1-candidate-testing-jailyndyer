3ab314e1984fa75083ed2ddb62b01b6a919af3f8
jdyer <>
2021-06-03 23:48:16 +0000

    Removing code for Part 2 of the assignment

--- candidate-testing.js
+++ candidate-testing.js
@@ -27,18 +27,6 @@ function askQuestion() {
  
   const input = require("readline-sync");
   candidateAnswer = input.question(question);
-  
-  //console.log("Correct Answer: " + correctAnswers[0]);
-  //questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
-  //correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
-  //candidateAnswers = ["", "", "", "", ""];
-  //const input = require("readline-sync");
-  
-  
-  //questions.forEach ( = 0; i < cars.length; i++)
-  //candidateAnswers[0] = input.question(questions[0]);
-  //console.log("Correct Answer: " + correctAnswers[0]);
-
 }
 
 function gradeQuiz(candidateAnswers) {
