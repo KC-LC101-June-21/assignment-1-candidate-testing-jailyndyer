1939f705a0713abe994ca4d8c51062e1bba123d0
jdyer <84543389+jailyndyer@users.noreply.github.com>
2021-06-14 19:19:22 +0000

    Initialized strings to "" in order to pass github testing

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,12 +5,12 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName //
 let candidateName;
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = "";
+let correctAnswer = "";
+let candidateAnswer = "";
 let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
-let candidateAnswers;
+let candidateAnswers = ["", "", "", "", ""];
 
 function askForName() {
   // // TODO 1.1b: Ask for candidate"s name //
@@ -20,12 +20,11 @@ function askForName() {
 
 function askQuestion(candidateAnswers) {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  candidateAnswers = ["", "", "", "", ""];
+  // candidateAnswers = ["", "", "", "", ""];
 
   for (i = 0; i < candidateAnswers.length; i++)
   {
     candidateAnswers[i] = input.question(questions[i]);
-    correctAnswer = correctAnswers[i];
     console.log(`Your answer: ${candidateAnswers[i]}`);
     console.log(`Correct Answer: ${correctAnswers[i]}\n`);
   }
